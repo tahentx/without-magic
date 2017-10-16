d3e14b9129230ed82a699d7ee494110e41ac3430
Todd Hendricks <hendricks.ta@gmail.com>
2017-10-15 17:38:11 -0700

    add numerical requirement to student show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'students' => 'students#index'
-  get 'students/:id' => 'students#show'
+  get 'students/:id' => 'students#show', as: 'student', id: /\d+/
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
