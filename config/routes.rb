19240313a2b98b27de110ac2c14ee47bf6b331b0
Todd Hendricks <hendricks.ta@gmail.com>
2017-10-15 17:10:55 -0700

    update route file to include index and show actions from students controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'students/index'
-
-  get 'students/show'
+  get 'students' => 'students#index'
+  get 'students/:id' => 'students#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
