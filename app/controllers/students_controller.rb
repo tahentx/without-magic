8cadac2a34623dad6913fefd6656c54891246f3c
Todd Hendricks <hendricks.ta@gmail.com>
2017-10-17 09:54:15 -0700

    routed and implemented student#new and student#create actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,4 +6,22 @@ class StudentsController < ApplicationController
   def show
   	@student = Student.find(params[:id])
   end
+
+  def new
+	@student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+    	redirect_to @student
+    else
+    	render :new
+    end
+  end
+
+  private 
+  def student_params
+	params.require(:student).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'students' => 'students#index'
   get 'students/:id' => 'students#show', as: 'student', id: /\d+/
   root to: 'students#index'
+  get 'students/new' => 'students#new', as: 'new_student'
+  post 'students' => 'students#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
